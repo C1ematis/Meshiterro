8473f6e8ee27eb63850a81fd639ed5252e972127
Clematis <khtafa@gmail.com>
2021-08-14 10:34:53 +0000

    Finish: Meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'de770b3acac5b91d21a7846dbf97985f131e69bc19d3789f730158cd2407833cf182a83fcff32c78d5b3ceab7379fbc57fe781e2cd0b4263c4e80de5305b6021'
